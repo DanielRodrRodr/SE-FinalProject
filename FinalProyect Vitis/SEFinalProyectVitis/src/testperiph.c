0720e82d59fcfa583d62280cc770994ff8a15880
maelhg01 <maelhg01@ucm.es>
2026-01-18 17:32:02 +0100

    Corregido matriz puntos, temporizador y puesto leds

--- FinalProyect Vitis/SEFinalProyectVitis/src/testperiph.c
+++ FinalProyect Vitis/SEFinalProyectVitis/src/testperiph.c
@@ -32,6 +32,9 @@
 #include "xil_cache.h"
 #include "matrizpuntos.h"
 #include "teclado.h"
+#include "copro.h"
+#include "altavoz.h"
+#include "ledsrgb.h"
 
 #define MILISEGUNDOS_PERIODO        200000   // 0,2 segundos de base para +1 en tiempo
 #define MAX_TIEMPO     90
@@ -44,16 +47,88 @@
 #define KEY_V_MAS     0x7 //(1,1)
 #define KEY_V_MENOS   0x11 //(2,1)
 
-int tiempo = 0;
+#define MILISEGUNDOS_PARPADEO 500000
+
+u32 tiempo = 0;
 int jugando = 0;
 int gol_local = 0;
 int gol_visitante = 0;
 
+u32 red_val = 0;
+u32 green_val = 0;
+u32 blue_val = 0;
+
+u32 red_signal = 0;
+u32 green_signal = 0;
+u32 blue_signal = 0;
+
+int tiempo_parpadeo = 0;
+int ciclos_parpadeo = 0;
+
+void update_leds() {
+   RGB_mWriteReg(XPAR_RGB_0_S00_AXI_BASEADDR, RGB_S00_AXI_SLV_REG0_OFFSET, red_val);
+   RGB_mWriteReg(XPAR_RGB_0_S00_AXI_BASEADDR, RGB_S00_AXI_SLV_REG1_OFFSET, green_val);
+   RGB_mWriteReg(XPAR_RGB_0_S00_AXI_BASEADDR, RGB_S00_AXI_SLV_REG2_OFFSET, blue_val);
+}
+
+
+void apagar_leds() {
+   red_val = 0;
+   green_val = 0;
+   blue_val = 0;
+   update_leds();
+}
+
+void parpadeo_color(u32 r, u32 g, u32 b) {
+   tiempo_parpadeo += MILISEGUNDOS_PERIODO;
+   if(tiempo_parpadeo > MILISEGUNDOS_PARPADEO){
+      tiempo_parpadeo = 0;
+      ciclos_parpadeo++;
+   }
+
+   if(ciclos_parpadeo % 2 == 0){ //leds encender
+      red_val = r; 
+      green_val = g; 
+      blue_val = b;
+      update_leds();
+   }
+   else{ //apagar leds
+      apagar_leds();
+   }
+}
+
+u8 convertir_char(char c){
+   u8 ret = 0x00;
+   switch (c) {
+      case '0': ret = 0x1F;break;
+      case '1': ret = 0x05;break;
+      case '2': ret = 0x17;break;
+      case '3': ret = 0x15;break;
+      case '4': ret = 0x0D;break;
+      case '5': ret = 0x19;break;
+      case '6': ret = 0x1B;break;
+      case '7': ret = 0x05;break;
+      case '8': ret = 0x1F;break;
+      case '9': ret = 0x1D;break;
+      case 'L': ret = 0x1C;break;
+      case 'V': ret = 0x0E;break;
+      case '-': ret = 0x02;break;
+      case ' ': ret = 0x00;break;
+      default:  ret = 0x00;
+   }
+
+   return ret;
+}
+
 
 //funcion escribir en matriz de puntos
 void escribir_char(int pos, char c){
-   MATRIZPUNTOS_mWriteReg(XPAR_MATRIZPUNTOS_0_S00_AXI_BASEADDR, MATRIZPUNTOS_S00_AXI_SLV_REG0_OFFSET, c);
-   MATRIZPUNTOS_mWriteReg(XPAR_MATRIZPUNTOS_0_S00_AXI_BASEADDR, MATRIZPUNTOS_S00_AXI_SLV_REG1_OFFSET, pos);
+   u32 paquete = 0;
+   u8 patron = convertir_char(c);
+
+   paquete = ((0 & 0x07) << 24) |   ((pos & 0x07) << 17) |  ((patron & 0x1F) << 8);
+
+   MATRIZPUNTOS_mWriteReg(XPAR_MATRIZPUNTOS_0_S00_AXI_BASEADDR, MATRIZPUNTOS_S00_AXI_SLV_REG0_OFFSET, paquete);
 }
 
 //escribir marcado, formato L0-0V 32
@@ -85,8 +160,13 @@ int main()
       keypad_data = TECLADO_mReadReg(XPAR_TECLADO_0_S00_AXI_BASEADDR, TECLADO_S00_AXI_SLV_REG0_OFFSET);
       key_code  = (keypad_data & 0x0F);
 
+      int local_ant = gol_local;
+      ubt vis_ant = gol_visitante;
       switch (key_code){
          case KEY_PAUSA:
+            if (tiempo == 0) {
+               COPRO_mWriteReg(XPAR_COPRO_0_S00_AXI_BASEADDR, COPRO_S00_AXI_SLV_REG3_OFFSET, MAX_TIEMPO);
+            }
             if (tiempo < MAX_TIEMPO)
                jugando = !jugando;
             break;
@@ -95,6 +175,8 @@ int main()
             gol_local = 0;
             gol_visitante = 0;
             jugando = 0;
+            COPRO_mWriteReg(XPAR_COPRO_0_S00_AXI_BASEADDR, COPRO_S00_AXI_SLV_REG3_OFFSET, 0);
+            apagar_leds();
             break;
          case KEY_L_MAS:
             if (gol_local < 9) gol_local++;
@@ -115,13 +197,46 @@ int main()
       // Limpiar teclado
       TECLADO_mWriteReg(XPAR_TECLADO_0_S00_AXI_BASEADDR, TECLADO_S00_AXI_SLV_REG0_OFFSET, 0x0);
           
-      //sumar contador
-      if (jugando && tiempo < MAX_TIEMPO) {
-         tiempo++;
-         if (tiempo == DESCANSO)
-            jugando = 0; // pausar en descanso
-       
+      //leds
+      if(gol_local > local_ant){
+         ciclos_parpadeo = 0;
+         red_signal = 0; 
+         green_signal = 0; 
+         blue_signal = 170;
       }
+
+       if (gol_visitante > gol_visitante_prev){
+          ciclos_parpadeo = 0;
+         red_signal = 0; 
+         green_signal = 170; 
+         blue_signal = 0;
+       }
+
+      //sumar contador
+      if (jugando) {
+         // leer contador hardware
+         tiempo = COPRO_mReadReg(XPAR_COPRO_0_S00_AXI_BASEADDR, COPRO_S00_AXI_SLV_REG3_OFFSET);
+      }
+
+      //descanso
+      if (tiempo == DESCANSO) {
+         red_signal = 170; 
+         green_signal = 170; 
+         blue_signal = 0;
+         update_leds(); // Amarillo
+      }
+      //fin partido
+      if (tiempo >= MAX_TIEMPO) { //rojo
+         red_val = 170; 
+         green_val = 0; 
+         blue_val = 0;
+         update_leds();
+      }
+
+      if(ciclos_parpadeo < 6){
+         parpadeo_color(red_signal, green_signal, blue_signal);
+      }
+
 
       mostrar_marcador();
       usleep(MILISEGUNDOS_PERIODO);
